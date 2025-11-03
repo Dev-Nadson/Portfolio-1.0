8a1030fff3fddb343db6db3628c4421a1a3fe825
Dev-Nadson <nadson.pros@gmail.com>
2025-11-03 18:58:27 -0300

    SVG Adicionado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,9 +8,17 @@ function Header() {
             <div className="flex justify-between bg-indigo-200 h-22 rounded-t-3xl ">
                 <div className="flex items-center mx-24 gap-5">
                     <Terminal className="w-10 h-10" />
-                    <h1 className="text-5xl font-bold font-serif">Nadson Alex</h1>
+                    <h3 className="text-3xl font-bold font-serif">Nadson Alex</h3>
                 </div>
                 <Section />
+            </div>
+            <div className="flex justify-between">
+                <div>
+                    <h1>Olá, meu nome é Nadson Alex!</h1>
+                    <p className="h-160 w-140 my-15 ml-65 text-center">
+                    </p>
+                </div>
+                <img src="../../../public/send-message.svg" alt="" className="h-150 my-15 mr-62" />
             </div>
         </header>
     )
