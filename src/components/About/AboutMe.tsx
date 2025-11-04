a7680d53e13d8a4f3d6e10cbe263a9efcd2d2469
Dev-Nadson <nadson.pros@gmail.com>
2025-11-04 18:59:59 -0300

    Sobre mim feito

--- src/components/About/AboutMe.tsx
+++ src/components/About/AboutMe.tsx
@@ -7,8 +7,8 @@ function Tech(props: Iprops) {
     const { name, src } = props
     return (
         <span className=" border w-55 h-23 border-purple-600 shadow-md shadow-purple-300 rounded-2xl p-4 flex items-center justify-center gap-4 hover:bg-purple-200 hover:ring-2 hover:ring-amber-50 transition-all duration-300 ease-in-out transform hover:scale-105">
-            <img className="size-15" src={src}></img>
-            <h3 className="text-lg">{name}</h3>
+            <img className="ml-2 size-15" src={src}></img>
+            <h3 className="text-lg font-mono text-center">{name}</h3>
         </span>
     )
 }
@@ -16,13 +16,19 @@ function Tech(props: Iprops) {
 function AboutMe() {
     return (
         <div id="sobremim" className=" h-[48vw] mt-12 mb-6 mx-9 border shadow-2xl shadow-purple-300 border-purple-700 rounded-3xl flex flex-col justify-between p-6">
-            <div>
-                <h1>Sobre Mim</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum quia ex aliquam omnis accusamus impedit eum numquam nulla quo corrupti, itaque autem tempora dolorem architecto debitis nam velit a laudantium!</p>
-                <h2>Tech stack:</h2>
+            <div className="mx-10 text-center">
+                <h1 className="text-[2.8rem] my-4 mt-10 font-bold">Sobre <strong className="text-indigo-700">Mim</strong>:</h1>
+                <p className=" mx-32 text-3xl mt-5">
+                    Sou desenvolvedor fullstack pernambucano, focado no desenvolvimento de aplicações escaláveis e com alta performance.
+                    Curso Ciência da Computação na Unifavip Wyden com bolsa integral pelo Prouni e atualmente foco meus estudos no ecossistema Node.js,
+                    que compõe a minha stack de desenvolvimento.
+                </p>
+                <h2 className="font-bold mt-12 text-[2.3rem]">
+                    <strong className="text-indigo-700">Tech</strong> Stack:
+                </h2>
             </div>
 
-            <div className=" flex justify-center mb-30">
+            <div className=" flex justify-center mb-20">
                 <div className="grid grid-cols-5 grid-rows-3 gap-15 ">
                     <Tech name="Typescript" src="TypeScript.svg" />
                     <Tech name="Javascript" src="JavaScript.svg" />
