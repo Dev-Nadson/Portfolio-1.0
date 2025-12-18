7fed49eadb424353dddc476ff36a4978c45890d3
Dev-Nadson <nadson.pros@gmail.com>
2025-12-18 13:02:00 -0300

    Refac: Ajustes na responsividade

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Code2 } from "lucide-react"
 
 function Header() {
     return (
-        <header className="h-200 md:h-[95vh] border shadow-xl shadow-purple-300 border-purple-700 rounded-3xl flex flex-col">
+        <header className="h-150 md:h-[95vh] border shadow-xl shadow-purple-300 border-purple-700 rounded-3xl flex flex-col">
 
             <div className="bg-purple-200 py-5 px-5 md:px-28 rounded-t-3xl transition-all duration-500">
                 <div className="mx-auto flex justify-between max-w-400">
@@ -16,23 +16,23 @@ function Header() {
             </div>
 
 
-            <section className="px-3 md:px-15 border flex flex-1 items-center justify-center gap-4">
+            <section className="px-4 md:px-15 flex flex-1 items-center justify-center gap-4">
 
                 <div className="flex items-center justify-center">
-                    <div className="flex flex-col gap-30 items-center md:items-start">
+                    <div className="flex flex-col justify-between items-center lg:items-start h-full min-h-90">
 
                         <div className="flex flex-col gap-4">
-                            <h1 className="text-3xl md:text-6xl text-center md:text-start">Olá, meu nome é <strong>Nadson Alex!</strong></h1>
-                            <h3 className="text-3xl md:text-5xl text-center md:text-start">Desenvolvedor <strong className="text-indigo-700">Fullstack</strong></h3>
-                            <p className="text-xl md:text-2xl text-center md:text-start">Transformando ideias em realidade de forma <strong>elegante e escalável</strong>! </p>
+                            <h1 className="text-4xl md:text-6xl text-center lg:text-start text-wrap">Olá, meu nome é <strong>Nadson Alex!</strong></h1>
+                            <h3 className="text-3xl md:text-5xl text-center lg:text-start">Desenvolvedor <strong className="text-indigo-700">Fullstack</strong></h3>
+                            <p className="text-xl md:text-2xl text-center lg:text-start">Transformando ideias em realidade de forma <strong>elegante e escalável</strong>! </p>
                         </div>
 
                         <div className="">
-                            <a href="#projetos" className=" flex w-fit bg-purple-300  py-4 px-18 rounded-full hover:shadow-lg hover:shadow-purple-400 hover:ring hover:ring-purple-50 text-2xl font-bold transition-all duration-300 ease-in-out transform hover:scale-110">Veja meus Projetos</a>
+                            <a href="#projetos" className=" flex w-fit bg-purple-300 py-4 px-10 md:px-18 rounded-full hover:shadow-lg hover:shadow-purple-400 hover:ring hover:ring-purple-50 text-xl md:text-2xl font-bold transition-all duration-300 ease-in-out transform hover:scale-110">Veja meus Projetos</a>
                         </div>
                     </div>
 
-                    <img src="/send-message.svg" alt="" className="h-[38vw] hidden md:block my-6" />
+                    <img src="/send-message.svg" alt="" className="h-[38vw] hidden lg:block my-6" />
                 </div>
 
             </section>
